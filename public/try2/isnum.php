95627960464996619f3637d850367cb3af5b2939
purin <140368239+momopurin89@users.noreply.github.com>
2025-04-15 02:38:42 +0000

    try2 変更

--- public/try2/isnum.php
+++ public/try2/isnum.php
@@ -8,10 +8,17 @@
   <body class="cyberpunk-bg">
     <div class="login-box">
       <h2>
-<?php
+      <?php
 
 
-echo "入力された数字は： " . $_GET['indata'];
+// 入力欄に無理やり配列が入っていないか確認する 
+$indata = filter_input(INPUT_GET,'indata');
+
+//正規表現を使って文字をチェック
+if(mb_ereg('^[0-9]+$',$indata) == false){
+  die("数字を入力してください。");
+}
+echo "入力された数字は： " . $indata;
 ?>
     </h2>
     </div>
