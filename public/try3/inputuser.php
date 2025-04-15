b424fe46f4a305285a016d4411667161c492f16a
purin <140368239+momopurin89@users.noreply.github.com>
2025-04-15 02:39:08 +0000

    try3変更

--- public/try3/inputuser.php
+++ public/try3/inputuser.php
@@ -9,6 +9,22 @@
     <div class="login-box">
       <h2>
 <?php
+
+//入力欄に無理やり配列が入っていないか確認する
+$username = filter_input(INPUT_GET,'username');
+$useraddress = filter_input(INPUT_GET,'useraddress');
+$usermail = filter_input(INPUT_GET,'usermail');
+
+//正規表現を使って文字をチェック
+if (!preg_match('/^[ぁ-んァ-ヶー一-龠々ー\s]+$/u', $username)) {
+  die("名前は日本語で入力してください。");
+}
+if (!preg_match('/^[ぁ-んァ-ヶー一-龠々ー0-9ー－、。 \s]+$/u', $useraddress)) {
+  die("住所は日本語で入力してください。");
+}
+if (!preg_match('/^[a-z0-9!#$%&\'*+\/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*$/i', $usermail)) {
+  die("正しい形式のメールアドレスを入力してください。");
+}
 echo "あなたが入力した値<br>";
 echo "名前：" . $_GET['username'] . "<br>";
 echo "住所：" . $_GET['useraddress']. "<br>";
